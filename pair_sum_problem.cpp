4ec2b54d9b4069af375fa8393afabdc4dd845110
Bhupen12 <68655036+Bhupen12@users.noreply.github.com>
2021-12-26 11:39:52 +0530

    Updating Code

--- pair_sum_problem.cpp
+++ pair_sum_problem.cpp
@@ -1,1 +1,33 @@
+// time complexity O(n)
+//given input a sorted array
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int pairSumPattern(int a[] , int n , int k){
+    int low = 0;
+    int high =n-1;
+
+    while(low<high){
+        if( a[low] + a[high] == k){
+            cout<<low<<" "<<high<<endl;
+            return 1;
+        }else if( a[low]+a[high] > k){
+            high--;
+        }else{
+            low++;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+    for(int i = 0 ; i<n; i++){
+        cin >> a[i];
+    }
+    pairSumPattern(a , n , k);
+    return 0;
+}
