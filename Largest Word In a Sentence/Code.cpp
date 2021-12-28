d3bd42b669766a31e7cdc18c5c230f4f38ad5e8b
Bhupen12 <68655036+Bhupen12@users.noreply.github.com>
2021-12-28 09:50:32 +0530

    longest word with its length in a sentence

--- Largest Word In a Sentence/Code.cpp
+++ Largest Word In a Sentence/Code.cpp
@@ -12,13 +12,16 @@ int main(){
 
     int i=0;
     int currLen = 0 , maxLen = 0;
+    int st=0 , maxst=0;
     while(1){
 
         if(arr[i]==' ' || arr[i] == '\0'){
             if(currLen > maxLen){
                 maxLen = currLen;
+                maxst = st;
             }
             currLen = 0;
+            st = i+1;
         }else{
             currLen++;
         }
@@ -29,5 +32,8 @@ int main(){
         i++;
     }
     cout<<maxLen<<" ";
+    for(int i=0 ; i< maxLen ; i++){
+        cout<<arr[i + maxst];
+    }
     return 0;
 }
